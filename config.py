c3da5164d22b41a87928267365003e0ec8fd1ae1
Saba Orkoshneli <47318592+SabaOrk@users.noreply.github.com>
2020-03-26 05:51:43 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#configuration file
+#everything here is required for scraper to work
+
+client = dict(
+    client_id = '',
+    client_secret = '',
+    username = '',
+    password = '',
+    user_agent = '' #can be random, example: user
+)
+subreddit = dict(
+    name = '', #ex: askreddit
+    limit = '', #ex: 3500
+    sortby = '' #ex: hot ,you can use: hot, new, top, rising, controversial
+)
